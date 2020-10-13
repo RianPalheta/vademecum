670d6b77ef1299645f99bac0ca705485b1945255
Rian Cristyan <palhetarian@gmail.com>
2020-10-13 17:17:58 -0300

    Backup

---
+++ views/home.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/corpo-style.css" type="text/css" />
+<main>
+		<div class="mains">
+			<div class="destaques">
+				<h2>Destaques:</h2>
+			</div>
+		</div>
+		<div id="codigos">
+			<div class="organizacao">
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cf" title="Constituição Federal"><img src="assets/images/cf.png"><h3>CF</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cc" title="Código Civil"><img src="assets/images/dc.png"><h3>CC</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cpc" title="Código Processual Civil"><img src="assets/images/cpc.png"><h3>CPC</h3></a>
+			</div>
+			<div class="organizacao">
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cp" title="Código Penal"><img src="assets/images/cp.png"><h3>CP</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cpp" title="Código Processual Penal"><img src="assets/images/cpp.png"><h3>CPP</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos cppm" title="Código Processual Penal Militar"><img src="assets/images/cppm.png"><h3>CPPM</h3></a>
+			</div>
+			<div class="organizacao">
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos clt" title="Consolidações das Leis Trabalhistas"><img src="assets/images/clt.png"><h3>CLT</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos ctb" title="Código de Trânsito Brasileiro"><img src="assets/images/ctb.png"><h3>CTB</h3></a>
+				<a href="<?php echo BASE_URL; ?>views/cf.php" class="codigos eca" title="Estatuto da Criança e do Adolescente"><img src="assets/images/eca.png"><h3>ECA</h3></a>
+			</div>
+		</div>
+</main>
